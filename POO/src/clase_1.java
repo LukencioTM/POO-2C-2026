dee28f5c074359e6c9cddcbb747b849ee0872b10
LukencioTM <000lr2015@gmail.com>
2026-09-10 19:12:44 -0300

    Cambios menores en clase_1.java

---
+++ POO/src/clase_1.java
@@ -0,0 +1,20 @@
+public class clase_1 {
+    public static void main(String[] args) {
+        int puntaje = 95;
+        char calificacion;
+
+        if (puntaje >= 90) {
+            calificacion = 'A';
+        } else if (puntaje >=80) {
+            calificacion = 'B';
+        } else if (puntaje >= 70) {
+            calificacion = 'B';
+        } else if (puntaje >= 60) {
+            calificacion = 'C';
+        } else {
+            calificacion = 'F';
+        }
+        System.out.println("Tu puntaje es: " + calificacion);
+    }
+}
+}
