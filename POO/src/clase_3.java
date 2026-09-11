13c1c6270529145fcd83ae414d6803f5ee125d6d
Luka <000lr2015@gmail.com>
2026-09-11 01:59:45 -0300

    Adicion de lo realizado al main

--- POO/src/clase_3.java
+++ POO/src/clase_3.java
@@ -5,20 +5,26 @@ public class clase_3 {
          * ...
          * } while (condicion);
          */
-        int numero = 28;
+        int numero = 496;
         int i = 1;
         int suma = 0;
 
         do  {
             if (numero % i == 0) {
                 suma += i;
+                System.out.println(i + " es un divisor de " + numero + ". suma total: " + suma);
             }
-        } while (i <= numero / 2); {
+            i++;
+        } while (i <= numero / 2); // Condicion que controla la repeticion del bucle
+        {
+            if (suma == numero) {
+                System.out.println(numero + " es un numero perfecto");
+            } else {
+                System.out.println(numero + " no es un numero perfecto");
+            }
 
         }
 
-
     }
-
 
 }
